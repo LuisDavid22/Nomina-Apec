00e958c4e4f392740a5328e4c45801b2ca17303b
bcsolutions2 <briancorrea0311@gmail.com>
2019-05-16 11:39:34 -0400

    arreglos

--- app.js
+++ app.js
@@ -1,67 +1,57 @@
 var fs = require('fs');
 var mongoose = require('mongoose');
 
-let texto = "";
-
 //Conexion con la BD
-mongoose.connect('mongodb://localhost/NominaApec', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/unapecDB', { useNewUrlParser: true });
 
 //Creando Schemas
 let EmpleadoSchema = new mongoose.Schema({
-    NoCuenta: String,
-    Cedula:String,
-    Sueldo:Number
-  });
+	NoCuenta: String,
+	Cedula: String,
+	Sueldo: Number
+});
 
-  let InfoApecSchema = new mongoose.Schema({
-    Rnc: String,
-    NoCuenta:String
-  }, { collection: 'infoApec' });
+let InfoApecSchema = new mongoose.Schema(
+	{
+		Rnc: String,
+		NoCuenta: String
+	},
+	{ collection: 'infoApec' }
+);
 
-  //Creando Modelos
-  var Empleado = mongoose.model('empleados', EmpleadoSchema);
-  var InfoApec = mongoose.model('InfoApec', InfoApecSchema);
+//Creando Modelos
+var Empleado = mongoose.model('empleados', EmpleadoSchema);
+var InfoApec = mongoose.model('InfoApec', InfoApecSchema);
 
 //Variables
-var textoCabecera = "";
-var textoDetalle = "";
+var textoCabecera = '';
+var textoDetalle = '';
 var d = new Date();
-var Fecha = d.toLocaleDateString();
+var Fecha = d.toLocaleDateString('es-ES');
 var MontoTotal = 0;
 var CantEmpleados = 0;
 
-  //Buscando datos de la cabecera
-  InfoApec.find(function (err, info) {
-    if (err) return console.error(err);
+//Buscando datos de la cabecera
+InfoApec.find(function(err, info) {
+	if (err) return console.error(err);
 
+	//Buscando datos de los empleados
+	Empleado.find(function(err, empleados) {
+		if (err) return console.error(err);
 
-  //Buscando datos de los empleados
-    Empleado.find(function (err, empleados) {
-        if (err) return console.error(err);
-    
-      for(var i in empleados){
-         textoDetalle += `${empleados[i].NoCuenta}\t ${empleados[i].Cedula}\t ${empleados[i].Sueldo.toLocaleString()}\n`
-         MontoTotal += empleados[i].Sueldo;
-         CantEmpleados ++;
-      }
+		for (var i in empleados) {
+			textoDetalle += `\n${empleados[i].NoCuenta}\t${empleados[i].Cedula}\t${empleados[i].Sueldo}`;
+			MontoTotal += empleados[i].Sueldo;
+			CantEmpleados++;
+		}
 
-      textoCabecera = `${info[0].Rnc}\t ${info[0].NoCuenta}\t ${Fecha}\t ${Fecha}\t ${MontoTotal.toLocaleString()}\n\n`
+		textoCabecera = `${info[0].Rnc}\t${info[0].NoCuenta}\t${Fecha}\t${Fecha}\t${MontoTotal}`;
 
-       fs.appendFile('Nomina.txt', ` ${textoCabecera}  ${textoDetalle} \n ${CantEmpleados} Empleados`, function (err) {
-    if (err) throw err;
-    console.log(` ${textoCabecera}  ${textoDetalle} \n ${CantEmpleados} Empleados`); 
-  }); 
-    
-      })
-  })
-
-
-
-  
-
-
-
- /* fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });*/
+		fs.writeFile('./../../Nomina.txt', `${textoCabecera}${textoDetalle}\n${CantEmpleados} Empleados`, function(
+			err
+		) {
+			if (err) throw err;
+			console.log(`${textoCabecera}  ${textoDetalle} \n${CantEmpleados} Empleados`);
+		});
+	});
+});
